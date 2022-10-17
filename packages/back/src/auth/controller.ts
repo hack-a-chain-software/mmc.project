6e822d411b36018606651125b7c9985a1814b3b7
Eduardo <eduardogbg@gmail.com>
2022-10-17 17:25:52 -0300

    fix(back): deserialize JSON body @ POST auth/login

--- packages/back/src/auth/controller.ts
+++ packages/back/src/auth/controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Post, Response } from '@nestjs/common';
+import { Body, Controller, Headers, Post, Response } from '@nestjs/common';
 import * as express from 'express';
 import { PublicKey } from 'near-api-js/lib/utils';
 import { AUTH_URI } from './constants';
 import { AuthService, SignedMessage } from './service';
 
 interface SignedMessageDto {
-  message: Uint8Array;
-  signature: Uint8Array;
+  message: number[];
+  signature: number[];
   publicKey: string;
 }
 
@@ -17,22 +17,20 @@ interface LoginDto {
 
 @Controller(AUTH_URI)
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @Post('login')
   async login(@Body() body: LoginDto, @Response() res: express.Response) {
-    let publicKey: PublicKey;
+    let signedMessage: SignedMessage;
     try {
-      publicKey = PublicKey.from(body.signedMessage.publicKey);
+      signedMessage = {
+        message: Uint8Array.from(body.signedMessage.message),
+        signature: Uint8Array.from(body.signedMessage.signature),
+        publicKey: PublicKey.fromString(body.signedMessage.publicKey),
+      };
     } catch (error) {
       return res.status(400).json({ success: false, error: error.toString() });
     }
-
-    const signedMessage: SignedMessage = {
-      message: body.signedMessage.message,
-      signature: body.signedMessage.signature,
-      publicKey,
-    };
 
     const authResult = await this.authService.authenticate(
       body.accountId,
