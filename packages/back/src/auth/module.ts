4c331a2bd4dc067c6c8773afda9dfc2f92cea82f
Eduardo <eduardogbg@gmail.com>
2022-10-19 12:13:16 -0300

    tests(back): provisorily version required WASMs

--- packages/back/src/auth/module.ts
+++ packages/back/src/auth/module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CodecModule } from 'src/codec/module';
+import { JwtModule } from 'src/jwt/module';
 import { NearModule } from 'src/near/module';
 import { AuthController } from './controller';
-import { JwtModule } from '../jwt/module';
 import { AuthService } from './service';
 
 @Module({
