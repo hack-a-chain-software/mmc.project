6842693d223f89b29189ed86c7a878c142a536da
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-11-07 20:13:31 -0300

    feat: eslint and prettier pre commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,60 @@
+// eslint-disable-next-line
+const { resolve } = require('path');
+
 module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: ['import', 'prettier'],
   env: {
     browser: true,
-    es2021: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
   },
-  globals: {
-    process: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    project: './tsconfig.json',
   },
-  plugins: ["react", "@typescript-eslint"],
   rules: {
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "no-extra-boolean-cast": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
+    'react/jsx-filename-extension': [0],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'jsx-a11y/href-no-hash': ['off'],
+    'max-len': [
+      'warn',
+      {
+        code: 80,
+        tabWidth: 2,
+        comments: 80,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: ['./packages/*/tsconfig.json'],
+      },
+      alias: {
+        map: [['@', resolve(__dirname, './packages/front')]],
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
