fec3ba2c725e80eef4b7371f0caa970de9eed1d8
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2022-11-18 11:32:44 -0300

    Update main.tsx

--- packages/front/src/main.tsx
+++ packages/front/src/main.tsx
@@ -1,7 +1,7 @@
 import './index.css';
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import { App, WalletSelectorModal } from '@/components';
+import { App, WalletSelectorModal, Fallback } from '@/components';
 import { WalletSelectorContextProvider } from '@/utils/context/wallet';
 import { RelayEnvironment } from './relay';
 import { RelayEnvironmentProvider } from 'react-relay';
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <WalletSelectorContextProvider>
       <RelayEnvironmentProvider environment={RelayEnvironment}>
-        <Suspense fallback={''}>
+        <Suspense fallback={<Fallback/>}>
           <App />
         </Suspense>
       </RelayEnvironmentProvider>
