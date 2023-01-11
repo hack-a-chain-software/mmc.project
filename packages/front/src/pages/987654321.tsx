08376ea9aaf3f3ad41a2d85818f8df1fd4f9bbda
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-01-11 16:57:05 -0300

    fix(front): lint

--- packages/front/src/pages/987654321.tsx
+++ packages/front/src/pages/987654321.tsx
@@ -46,21 +46,11 @@ export const Play = () => {
 				{!loading && (
 					<motion.div
 						key="play-motion-hero"
-						initial={{
-							clipPath: 'circle(0% at 50vw 50vh)',
-						}}
-						animate={{
-							clipPath: 'circle(100% at 50vw 50vh)',
-						}}
-						exit={{
-							clipPath: 'circle(0% at 50vw 50vh)',
-						}}
-						transition={{
-							duration,
-						}}
-						className="
-              w-screen min-h-screen relative
-            "
+						initial={{ clipPath: 'circle(0% at 50vw 50vh)' }}
+						animate={{ clipPath: 'circle(100% at 50vw 50vh)' }}
+						exit={{ clipPath: 'circle(0% at 50vw 50vh)' }}
+						transition={{ duration }}
+						className="w-screen min-h-screen relative"
 					>
 						<div className="absolute px-[30px] w-full max-w-[1340px] pt-[32px] z-[9] flex items-center justify-end space-x-[32px] left-1/2 -translate-x-1/2">
 							<button
