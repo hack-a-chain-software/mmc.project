af8e31c122f23ebf2847c936146cb4b0e26dcdfe
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-01-28 00:03:05 -0300

    Update api.ts

--- packages/front/src/services/api.ts
+++ packages/front/src/services/api.ts
@@ -2,19 +2,22 @@ import axios from 'axios';
 import { apiUrl } from '@/constants/env';
 
 export const api = axios.create({
-  baseURL: apiUrl,
-  headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-  },
+	baseURL: apiUrl,
+	headers: {
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin': '*',
+	},
 });
 
-api.interceptors.response.use((response) => response, error => {
-  if (error.response.status !== 401) {
-    return error;
-  }
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error?.response?.status !== 401) {
+			return error;
+		}
 
-  window.location.reload();
-});
+		window.location.reload();
+	}
+);
 
 export default api;
