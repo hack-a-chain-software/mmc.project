035b947663eb3f5bed36662b3e78bbb9e57a9d90
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-02-05 14:43:16 -0300

    Update menu.tsx

--- packages/front/src/components/wallet/menu.tsx
+++ packages/front/src/components/wallet/menu.tsx
@@ -89,7 +89,7 @@ export const WalletMenu = () => {
             "
           >
             {
-              !!autenticated ? (
+              autenticated && accountId ? (
                 <>
                   <div className="py-[14px] px-[24px]">
                     <CopyToClipboard
