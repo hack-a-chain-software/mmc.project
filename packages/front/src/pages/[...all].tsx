c4bcf95c88ce31c909beafe82254832f4229e8e8
Mateus Santana <42101423+1Mateus@users.noreply.github.com>
2023-04-03 13:43:08 -0300

    feat(front): create 404 page

---
+++ packages/front/src/pages/[...all].tsx
@@ -0,0 +1,23 @@
+export const All = () => (
+  <div
+    className="bodypage bg-blue-100 w-screen h-screen flex items-center justify-center"
+  >
+    <div
+      className="flex flex-col items-center text-white"
+    >
+      <span
+        className="text-[90px]"
+      >
+        404
+      </span>
+      <span
+        className="text-xl"
+      >
+        Oops, page not found
+      </span>
+
+    </div>
+  </div>
+);
+
+export default All;
